73cf96c9e9e5c5d694484b87f5b091a60efbc632
Paulo Pinho <paulopinho.pluri@gmail.com>
2020-09-04 10:15:25 -0300

    Add migration, model, controller and route.

---
+++ app/Http/Controllers/MotoristasController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Motorista;
+use Illuminate\Http\Request;
+
+class MotoristasController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Motorista  $motorista
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Motorista $motorista)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Motorista  $motorista
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Motorista $motorista)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Motorista  $motorista
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Motorista $motorista)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Motorista  $motorista
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Motorista $motorista)
+    {
+        //
+    }
+}
---
+++ database/migrations/2020_09_04_121650_create_motoristas_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMotoristasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('motoristas', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nome', 100);
+            $table->char('cpf', 11);
+            $table->string('email', 320);
+            $table->enum('situacao', ['livre', 'em curso', 'retornando']);
+            $table->enum('status', ['ativo', 'inativo']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('motoristas');
+    }
+}
